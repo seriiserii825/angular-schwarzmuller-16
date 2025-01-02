3779c28822a2e3677bb38744dde8ac0704625458
serii burduja <seriiburduja@gmail.com>
2025-01-02 15:46:57 +0200

    feat: finished part 1

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
+  server_name = '';
+  servers = ['Testserver', 'Testserver 2', 'Testserver 3'];
+  server_details = false;
+  log_output: string[] = [];
 
+  toggleDetails() {
+    this.log_output.push(new Date().getTime().toString());
+    this.server_details = !this.server_details;
+  }
+
+  getBgColor(index: number) {
+    return index > 4 ? 'lightblue' : '';
+  }
+
+  addServer() {
+    this.servers.push(this.server_name);
+    this.server_name = '';
+  }
 }
