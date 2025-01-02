be5835b286bb0c20102b5a126ce31bb68da2ea2c
serii burduja <seriiburduja@gmail.com>
2025-01-02 17:40:22 +0200

    feat: recipes and header container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
 import {FormsModule} from '@angular/forms';
+import { TheBasicsComponent } from './pages/the-basics/the-basics.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './components/recipes/recipes-detail/recipes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
-    ServerComponent
+    ServerComponent,
+    TheBasicsComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/the-basics/the-basics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-the-basics',
+  templateUrl: './the-basics.component.html',
+  styleUrls: ['./the-basics.component.css']
+})
+export class TheBasicsComponent {
+
+}
