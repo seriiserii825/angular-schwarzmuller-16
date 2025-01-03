54f07bd697ccb7d39103136bc1426cf4a7dd346c
serii burduja <seriiburduja@gmail.com>
2025-01-03 16:44:32 +0200

    feat: show list accounts

--- src/app/components/account/list-account/list-account.component.ts
+++ src/app/components/account/list-account/list-account.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {IAccount} from 'src/app/interfaces/IAccount';
 
 @Component({
   selector: 'app-list-account',
   templateUrl: './list-account.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ListAccountComponent {
-
+  @Input() accounts: IAccount[] = [];
 }
